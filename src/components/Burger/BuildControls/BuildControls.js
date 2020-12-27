36409b55d72f02f29935802a7d67bc08eca33950
dev <devanshbhuptani@gmail.com>
2020-12-27 19:58:04 +0530

    Created BuildControls and BuildControl components with its CSS files.

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {  } from "./BuildControls.css";
+
+const buildControls = props => {
+    <div>
+        
+    </div>
+}
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 0,
+            salad: 1,
             bacon: 0,
             cheese: 0,
             meat: 0
