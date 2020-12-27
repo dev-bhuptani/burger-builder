d0078022651e84b3f036a850cc8b6b90e4275106
dev <devanshbhuptani@gmail.com>
2020-12-27 18:30:35 +0530

    Added state to BurgerBuilder component to manage ingredients. Also, developed Burger component.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,9 +3,16 @@ import "../Burger/Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+    const ingredients = Object.keys(props.ingredients).map(ingrd => {
+        return [...Array(props.ingredients[ingrd])].map((_, i) => {
+            return <BurgerIngredient key={ingrd + i} type={ingrd} />;
+        });
+    });
+
     return (
         <div className="Burger">
             <BurgerIngredient type="bread-top" />
+            {ingredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Aux from '../../hoc/Auxillary';
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 1
+        }
+    };
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>BurgerControls</div>
             </Aux>
         );
