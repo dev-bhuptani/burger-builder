3be5d1939b5f038dc56ba0343bf4c5fc9386495f
dev <devanshbhuptani@gmail.com>
2020-12-29 02:03:23 +0530

    Created Toolbar component with its CSS file in Navigation folder. Also, implemented it in Layout component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Auxillary";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 import "./Layout.css";
 
 const layout = props => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className="Layout">
             {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Toolbar.css";
+
+const toolbar = props => (
+    <header className="Toolbar">
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+        </nav>
+    </header>
+);
+
+export default toolbar;
